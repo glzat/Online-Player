3860251a68b0d204bddd9c8b0788ef7e7c77105d
Sakura Xiao <zsqdglz@163.com>
2024-02-05 11:32:04 +0800

    Fixed some bugs

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
  You can know how many online players the game has.
 ## How to use
  After initialization, enter the game name to view the number of online players.
- Say "quit" to exit.
+ Say `quit` to exit.
+## Debug Mode
+ If you want to get more data, use the debug mode! Enter `Debug` or `debug`, then enter the number of pages you want to expand, and you can get more data!
+# 中文翻译：
+ 你可以知道这个游戏有多少在线玩家
+## 使用方法
+ 在初始化后，输入游戏名称来查询在线玩家数量
+## 测试模式
+ 如果你想获得更多的数据（或您想查询的游戏未找到），可以输入`Debug`或者`debug`，之后输入你要拓展的页数，即可获得更多的数据！
--- main.py
+++ main.py
@@ -8,17 +8,9 @@ def find_in_matrix(text):
     for i in range(len(games)):
         if games[i][0] == text:
             return i
-try:
-    with open("data.csv","r",encoding="utf-8") as f:
-        if(f.readline() == ""):
-            raise FileNotFoundError
-        f.seek(0)
-        for line in f:
-            games.append(line.strip().split(","))
-except FileNotFoundError:# 第一次运行
-    print("[LOG] First time to run this program. Start crawling...\n")
+def load_data(start_page=1,pages=10):
     with open("data.csv","w",encoding="utf-8") as f:
-        for page_num in range(1,11):
+        for page_num in range(start_page,pages+1):
             url = f"https://steamcharts.com/top/p.{page_num}"
             while True:
                 try:
@@ -45,11 +37,24 @@ except FileNotFoundError:# 第一次运行
                     games.append([game_name,game_current_players,game_peak_players,game_hours_played])
             print(f"Successfully saved the Page{page_num}")
     print("All done!")
+    return
+
+try:
+    with open("data.csv","r",encoding="utf-8") as f:
+        if(f.readline() == ""):
+            raise FileNotFoundError
+        f.seek(0)
+        for line in f:
+            games.append(line.strip().split(","))
+except FileNotFoundError:# 第一次运行
+    print("[LOG] First time to run this program. Start crawling...\n")
+    load_data()
 
 while True:
     game_name = input("Please enter the game name: ")
-    if game_name == "Debug" or game_name == "debug":
-        print(games)
+    if game_name == "Debug" or game_name == "debug":# 手动调整、测试bug用，不会在正式版中出现
+        extension = int(input("请输入要拓展的页数，可以获得更多数据:")) 
+        load_data(11,10+extension)  
         break
     # 输入修正
     game_name = game_name.title()
@@ -61,4 +66,4 @@ while True:
             i = find_in_matrix(game_name) 
             print(f"Current players: {games[i][1]}\nPeak players: {games[i][2]}\nHours played: {games[i][3]}")
         except TypeError:
-            print("The game is not found.")
+            print("未找到游戏,请检查输入是否正确。或使用Debug模式拓展数据。")
